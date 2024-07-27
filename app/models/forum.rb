d19599de9348d4a5592be478676791dbac73a178
exit <zala.hetalba@gmail.com>
2024-07-27 18:26:16 -0400

    tested commit for lesson7_1

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,2 +1,10 @@
 class Forum < ApplicationRecord
+    has_many :posts
+    has_many :subscriptions
+    has_many :users, through: :subscriptions
+
+    # add in app/models/user.rb
+    has_many :subscriptions
+    has_many :posts
+    has_many :forums, through: :subscriptions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-    validates :skill_level, inclusion: { in: %w(beginner intermediate expert) }
+   # add in app/models/user.rb
+   has_many :subscriptions
+   has_many :posts
+   has_many :forums, through: :subscriptions
+   validates :skill_level, inclusion: { in: %w(beginner intermediate expert) }
 end
