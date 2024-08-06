3e2670a28c9a8022f7b37f616d2803aa2c334252
exit <zala.hetalba@gmail.com>
2024-08-06 18:01:01 -0400

    Created new branch  lesson7_2

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -3,8 +3,5 @@ class Forum < ApplicationRecord
     has_many :subscriptions
     has_many :users, through: :subscriptions
 
-    # add in app/models/user.rb
-    has_many :subscriptions
-    has_many :posts
-    has_many :forums, through: :subscriptions
+ 
 end
